2dab52dfd5dc3c45deb8cb18c74bc8d556ae498a
Alex Stout <alexstout009@hotmail.com>
2019-05-19 22:17:30 +0100

    created response api

---
+++ util/response.js
@@ -0,0 +1,32 @@
+const headers = {
+  'X-Content-Type-Options': 'nosniff',
+  'access-control-allow-headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+  'access-control-allow-methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+  'access-control-allow-origin': '*',
+}
+
+module.exports = {
+  send: (callback, data) => callback(null, {
+    statusCode: 200,
+    isBase64Encoded: false,
+    headers,
+    body:{
+      success: true,
+      data,
+    },
+  }),
+  error: (callback, err, code=500) => {
+    if (!err || err == {}) {
+      err = 'Unexpected error occurred.';
+    }
+    callback(null, {
+      statusCode: code,
+      isBase64Encoded: false,
+      headers,
+      body: {
+        success: false,
+        error: err,
+      },
+    });
+  },
+};
